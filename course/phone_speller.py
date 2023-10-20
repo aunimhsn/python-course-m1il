3dcefd3a6edd6f148288e4069b31e4ecf9d032df
Aunim HASSAN <aunim.hassan@gmail.com>
2023-10-20 14:18:33 +0200

    exo dict

---
+++ course/phone_speller.py
@@ -0,0 +1,21 @@
+phone = "+33265987412"
+result = ""
+
+digits_mapping = {
+    "0": "Zero",
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+}
+
+for digit in phone:
+    result += digits_mapping.get(digit, "[Character not mapped]") + ' '
+
+
+print(result)
