96709c2d4b87289bb31293f60f993ea743d2345c
Aunim HASSAN <aunim.hassan@gmail.com>
2023-10-20 14:19:56 +0200

    Update count_letters.py

--- course/count_letters.py
+++ course/count_letters.py
@@ -12,3 +12,31 @@ for char in alphabet:
     result[char] = text.count(char)
 
 print(result)
+
+
+# Solution 1
+# result = {}
+# text = text.lower()
+# for char in text:
+#     if char in result.keys():
+#         result[char] += 1
+#     else:
+#         result[char] = 1
+
+# print(result)
+
+
+# Solution 2 - count()
+# result = {}
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# for char in alphabet:
+#     result[char] = text.lower().count(char)
+
+# print(result)
+
+
+# Solution 3 - collections.Counter()
+# from collections import Counter
+# result = Counter(text.lower())
+
+# print(result)
